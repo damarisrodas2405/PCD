51cb3b45835dbe941034d4c059eda6e58b98b7d7
Damaris Rodas <dawirodas2019@gmail.com>
2026-03-08 21:52:17 -0600

    Implementa procesamiento de lineas y suma

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import sys
 
+#Limpiar valores
 def limpiar_valor(valor):
-
-    valor = valor.strip()
+    valor  = valor.strip()
     caracteres_validos = "0123456789.-"
     resultado = ""
 
     for char in valor:
         if char in caracteres_validos:
             resultado += char
-
     return resultado
 
-
+#Procesar lineas
 def procesar_linea(linea):
-
     linea = linea.strip()
 
     if linea == "":
         return 0
-
+    
     valores = linea.split(",")
     suma = 0
 
-    for valor in valores:
-
-        valor_limpio = limpiar_valor(valor)
+    for valores in valores:
+        valor_limpio = limpiar_valor()
 
         if valor_limpio == "":
             numero = 0
@@ -34,11 +31,8 @@ def procesar_linea(linea):
                 numero = int(float(valor_limpio))
             except ValueError:
                 numero = 0
-
         suma += numero
-
     return suma
-
 
 def main():
 
@@ -46,6 +40,8 @@ def main():
         resultado = procesar_linea(linea)
         print(resultado)
 
-
 if __name__ == "__main__":
     main()
+
+
+#Programa para procesar lineas y sumar valores
